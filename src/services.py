ff4e55e13c5533851666c7cca986d67507393310
novel <18.novel.18@gmail.com>
2023-11-14 10:07:09 +0300

    fix

--- src/services.py
+++ src/services.py
@@ -1,4 +1,4 @@
-from config import settings
+from src.config import settings
 from src.iex import IEXCloudClient
 from src.repositories import CurrencyRepository
 from src.schemas import CurrencyDetail
